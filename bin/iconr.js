6d9af96bdd62520fac9ca108b64da6c3b0a740fd
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-01-10 14:57:26 -0500

    delete all the stuffs

--- bin/iconr.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// modules
-var path = require('path'),
-    argv = require('optimist').argv,
-    cli = require(path.resolve(__dirname, '..', 'lib', 'cli'));
-
-// init cli
-cli(argv);
