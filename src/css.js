c800ec5dc895e22143b0068bc44b6843f926bfa2
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2016-04-25 07:00:00 -0400

    fix some lint errors

--- src/css.js
+++ src/css.js
@@ -8,7 +8,7 @@ module.exports = {
   // returns a string that can be saved as a CSS file
   createRules: (results, opts) => {
     const cssClassnamePrefix = opts.classname !== null ? opts.classname : '';
-    let css = [];
+    const css = [];
     let cssSvg = '';
     let cssNoInlineSvg = '';
     let cssNoDataUri = '';
@@ -17,14 +17,15 @@ module.exports = {
     }
 
     _.each(results, (result) => {
-      let height = !isNaN(result.height) ? 'height:' + result.height + 'px;' : '';
-      let width = !isNaN(result.width) ? 'width:' + result.width + 'px;' : '';
-      cssSvg += '.' + (cssClassnamePrefix + result.name) + '{' + height + width + 'background-image:url(' + result.svgdatauri + ');}';
+      const height = !isNaN(result.height) ? `height: ${result.height}px;` : '';
+      const width = !isNaN(result.width) ? `width: ${result.width}px;` : '';
+      const className = cssClassnamePrefix + result.name;
+      cssSvg += '.' + className + '{' + height + width + 'background-image:url(' + result.svgdatauri + ');}';
       if (!opts.nopngdata) {
-        cssNoInlineSvg += '.no-inlinesvg .' + (cssClassnamePrefix + result.name) + '{background-image:url(' + result.pngdatauri + ');}';
+        cssNoInlineSvg += '.no-inlinesvg .' + className + '{background-image:url(' + result.pngdatauri + ');}';
       }
       if (!opts.nopng && !opts.stdout) {
-        cssNoDataUri += '.no-datauri .' + (cssClassnamePrefix + result.name) + '{background-image:url(' + result.pngpath + ');}';
+        cssNoDataUri += '.no-datauri .' + className + '{background-image:url(' + result.pngpath + ');}';
       }
     });
 
@@ -46,30 +47,26 @@ module.exports = {
   // saves CSS file(s) to disk
   // can't use arrow syntax here because of "this" reference
   save: function saveCss(filename, cssArr, opts) {
-
     let css;
 
     // save separate css files
     if (opts.separatecss) {
-
       // prettify the CSS if necessary
       css = opts.pretty ? cssArr.map((cssStr) => pretty(cssStr)) : css;
 
       // save CSS with SVG data URIs
-      fs.writeFileAsync(filename + '.css', css[0]);
+      fs.writeFileAsync(`${filename}.css`, css[0]);
 
       // save CSS with fallback PNG data URIs
       if (!opts.nopngdata && (css[1].length !== 0)) {
-        fs.writeFileAsync(filename + '-noinlinesvg.css', css[1]);
+        fs.writeFileAsync(`${filename}-noinlinesvg.css`, css[1]);
       }
 
       // save CSS with fallback PNG image paths
       if (!opts.nopng && (css[2].length !== 0)) {
-        fs.writeFileAsync(filename + '-nodatauri.css', css[2]);
+        fs.writeFileAsync(`${filename}-nodatauri.css`, css[2]);
       }
-
     } else {
-
       // combine CSS into string
       css = this.munge(cssArr);
 
@@ -79,11 +76,10 @@ module.exports = {
       }
 
       // write CSS into a single file
-      fs.writeFileAsync(filename + '.css', css);
-
+      fs.writeFileAsync(`${filename}.css`, css);
     }
 
     return;
-  }
+  },
 
 };
