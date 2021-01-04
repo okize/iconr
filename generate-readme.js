7fb5448b8b072d2e5d471e2ab0f750589df5fcb8
Morgan Wigmanich <mwigmanich@wistia.com>
2021-01-04 08:00:00 -0400

    autofix lint errors

--- generate-readme.js
+++ generate-readme.js
@@ -6,9 +6,7 @@ const json2markdown = require('json2markdown'); // eslint-disable-line import/no
 const readmeTemplate = fs.readFileSync('./generate-template-readme.md', 'utf8');
 const project = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 const helpOptions = JSON.parse(fs.readFileSync('./lang/options.json', 'utf8'));
-const helpText = helpOptions.map(({ longName, shortName, description }) => {
-  return { longName, shortName, description };
-});
+const helpText = helpOptions.map(({ longName, shortName, description }) => ({ longName, shortName, description }));
 const helpTextMarkdown = json2markdown(helpText);
 const newReadme = readmeTemplate
   .replace('<%= description %>', project.description)
